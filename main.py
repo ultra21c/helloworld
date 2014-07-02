763036822026ef3caaac8941df1f02ca23f8498a
JungPilWon <ultra21c@gmail.com>
2014-07-02 23:02:38 +0900

    add

--- main.py
+++ main.py
@@ -3,7 +3,11 @@
 import sys
 import lib.send
 
-def print-helloworld():
+def ilove_jiwoo():
+	print 'i love jiwoo'
+	print 'I want to earn a lot of money'
+
+def print_helloworld():
 	print 'hello world'
 
 def calc(num):
@@ -16,6 +20,7 @@ def main(name):
 	print 'hello world'
 	lib.send.echo('my prog name is %s'%name)
 	calc(10)
+	ilove_jiwoo()
 
 if __name__ == '__main__':
 	name = sys.argv[0]
