8723987602cf651679631b38347118ca562640da
JungPilWon <ultra21c@gmail.com>
2014-07-02 22:55:16 +0900

    add calc

--- main.py
+++ main.py
@@ -3,9 +3,16 @@
 import sys
 import lib.send
 
+def calc(num):
+	sum = 0
+	for i in range(num):
+		sum+=i
+	print sum
+		
 def main(name):
 	print 'hello world'
 	lib.send.echo('my prog name is %s'%name)
+	calc(10)
 
 if __name__ == '__main__':
 	name = sys.argv[0]
